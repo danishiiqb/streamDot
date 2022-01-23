2efc60cfa37adeae376c2dd147ccf72ee3901f5b
Danish Iqbal <danishiiqbal82@gmail.com>
2022-01-23 13:57:06 +0530

    added footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer
+      className='footer mt-4'
+      style={{
+        textAlign: 'center',
+        marginBottom: '3rem',
+        fontSize: '2rem',
+        fontWeight: 'bold',
+      }}
+    >
+      Made By Danish Iqbal
+    </footer>
+  );
+}
+
+export default Footer;
