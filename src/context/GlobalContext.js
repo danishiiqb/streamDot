ce2f38379833d191bf466705f22aa1167e38013d
Danish Iqbal <danishiiqbal82@gmail.com>
2022-01-31 21:08:00 +0530

    added globalContext.js

---
+++ src/context/GlobalContext.js
@@ -0,0 +1,49 @@
+import { createContext, useContext, useEffect, useRef, useState } from 'react';
+import useWindow from '../hooks/useWindow';
+import { useDb } from './dbContext';
+
+const GlobalCntxt = createContext(null);
+const useGlobal = () => {
+  return useContext(GlobalCntxt);
+};
+
+const GlobalCntxtProvider = ({ children }) => {
+  const loade = useRef(false);
+  const { getUserfromdb, updateUidDocs } = useDb();
+  const [update, setUpdate] = useState(0);
+  const [UserSelected, setUserSelected] = useState(
+    JSON.parse(localStorage.getItem('currMainUser'))
+  );
+  const [routingNormal, setRoutingNormal] = useState(false);
+  const [siz] = useWindow();
+  useEffect(() => {
+    if (UserSelected) {
+      localStorage.setItem('currMainUser', JSON.stringify(UserSelected));
+    }
+  }, [UserSelected]);
+
+  useEffect(() => {
+    if (update) {
+      getUserfromdb(UserSelected.connUid).then(([res]) => {
+        let connAcc = res.connectedAcc.map((el) => {
+          return el.cnid === UserSelected.cnid ? UserSelected : el;
+        });
+        updateUidDocs(UserSelected.mainDocId, connAcc);
+      });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [update]);
+
+  const gb = {
+    loader,
+    routingNormal,
+    setRoutingNormal,
+    UserSelected,
+    setUserSelected,
+    setUpdate,
+    siz,
+  };
+  return <GlobalCntxt.Provider value={gb}>{children}</GlobalCntxt.Provider>;
+};
+
+export { GlobalCntxtProvider, useGlobal };
