4af38c590e1d8daf3bede98ac8ae275b535c2f7b
Danish Iqbal <danishiiqbal82@gmail.com>
2022-02-07 16:34:58 +0530

    added watchDash.js

---
+++ src/pages/WatchDash.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import WatchMain from '../components/watchDash.js/WatchMain';
+
+function WatchDash() {
+  return (
+    <div className='container'>
+      <WatchMain></WatchMain>
+    </div>
+  );
+}
+
+export default WatchDash;
